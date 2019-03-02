55b3b4e3241dc8aef237f3a9ccd10c11616f2694
12123ghh <1148564545@qq.com>
2019-03-02 14:38:57 +0800

    create paper def for teacher

--- app/controllers/teacher/papers_controller.rb
+++ app/controllers/teacher/papers_controller.rb
@@ -1,2 +1,24 @@
 class Teacher::PapersController < ApplicationController
+
+	def new
+		@teacher=User.teacher.find(params[:id])	
+		@paper=Paper.new	
+	end
+
+	def create
+		@teacher=User.teacher.find(params[:id])
+		@pape=Papers.new
+		if @paper.update(paper_params)
+			flash[:success]="success create paper"
+		else
+			render 'new'
+		end
+	end
+
+
+	private 
+
+	def paper_params
+		params.require(:paper).permit(:title,:subject,:questions_number,:creator_id)
+	end
 end
