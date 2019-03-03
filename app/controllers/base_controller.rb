338d5b4998903347691c442224833c411eb09a46
12123ghh <1148564545@qq.com>
2019-03-03 17:23:15 +0800

    检查用户是否登录-change base_controller.rb

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,7 +2,7 @@ class BaseController < ApplicationController
   def check_user_login_status
     if !current_user
       flash[:danger] = "must login!"
-      redirect_to new_admin_session_path
+      redirect_to login_path
     end
   end
 
