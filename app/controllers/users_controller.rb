dc35da69112889385de270c56d990f0299e08edd
12123ghh <1148564545@qq.com>
2019-03-03 16:12:51 +0800

    student登录注册

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user=User.new(user_params)
+  	@user=User.student.new(user_params)
   	if @user.save
   	  log_in @user
   	  flash[:success]="wecome!"
