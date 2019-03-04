1a265d0cdb708b7a10ea5e1c7fb22b7cdb969509
12123ghh <1148564545@qq.com>
2019-03-04 09:13:46 +0800

    学生考试功能

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -3,6 +3,7 @@ class Exam < ApplicationRecord
   belongs_to :user, -> { student }
 
   has_many :answers
+  accepts_nested_attributes_for :answers
 
   enum state: {pending: 1, finished: 2, close: 3}
 end
