6ecb9ea560f881c125fd35494cf52336f9e7803e
12123ghh <1148564545@qq.com>
2019-03-05 22:29:15 +0800

    教师信息edit-page

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,6 +1,6 @@
 class Paper < ApplicationRecord
   belongs_to :creator, class_name: "User"
-  has_many :questions
+  has_many :questions, dependent: :destroy
   has_many :exams
 
   accepts_nested_attributes_for :questions, allow_destroy: true
