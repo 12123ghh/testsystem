583cc2b29dcd63c0c5a7ed3e3efa5fba46334442
12123ghh <1148564545@qq.com>
2019-03-06 20:13:42 +0800

    教师个人信息查看与修改

--- app/controllers/teacher/sessions_controller.rb
+++ app/controllers/teacher/sessions_controller.rb
@@ -1,4 +1,4 @@
-class Teacher::SessionsController < ApplicationController
+class Teacher::SessionsController < Teacher::BaseController
   def new
 
   end
@@ -7,32 +7,11 @@ class Teacher::SessionsController < ApplicationController
   	user=User.teacher.find_by(email:params[:session][:email].downcase)
   	if user&&user.authenticate(params[:session][:password])
   	  log_in(user)
-  	  redirect_to teacher_teacher_user_path(user)
+  	  redirect_to teacher_teacher_user_path
   	else
   	  flash.now[:danger]='invalid email/password combination'
   	  render 'new'
   	end
   end
 
-  def destroy
-    log_out
-    redirect_to root_url
-  end
-
-  def log_in(user)
-    session[:user_id]=user.id
-  end
-
-  def current_user
-    @current_user||=User.find_by(id:session[:user_id])
-  end
-
-  def current_user?(user)
-    user==current_user
-  end
-
-  def log_out
-    session.delete(:user_id)
-    @current_user=nil
-  end
 end
--- app/controllers/teacher/teacher_users_controller.rb
+++ app/controllers/teacher/teacher_users_controller.rb
@@ -1,5 +1,5 @@
 class Teacher::TeacherUsersController < Teacher::BaseController
-  before_action :check_teacher_login_status, only: [:show, :edit, :update, :edit_password, :update_password]
+  before_action :check_teacher_login_status, only: [:show, :edit, :update]
 
   def new
   	@user=User.new
@@ -17,16 +17,13 @@ class Teacher::TeacherUsersController < Teacher::BaseController
   end
 
   def show
-  	@user=User.find(params[:id])
+  	@user = current_user
   end
 
   def edit
-  	@user=User.find(params[:id])
+  	@user = current_user
   end
 
-  def edit_password
-  	@user=User.find(params[:id])
-  end
 
   def update
   	if @user.update_attribute(update_params)
@@ -38,17 +35,6 @@ class Teacher::TeacherUsersController < Teacher::BaseController
   	end
   end
 
-  def update_password
-  	@user=User.find(params[:id])
-  	if @user.update_attributes(password_params)
-  	  flash[:success]="success!"
-  	  redirect_to teacher_teacher_user_path(@user)
-  	else
-  	  flash.now[:danger]='invalid password/password_confirmation combination'
-  	  render 'edit_password'
-  	end
-  end
-
   private 
 
   def teacher_params
@@ -56,11 +42,4 @@ class Teacher::TeacherUsersController < Teacher::BaseController
       					:password_confirmation)
   end
 
-  def update_params
-  	params.require(:user).permit(:name,:email)
-  end
-
-  def password_params
-  	params.require(:user).permit(:password,:password_confirmation)
-  end
 end
