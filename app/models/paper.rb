b5ca6f00e8d8ff2159024fd2a1c0e71a1ba09e4d
dino <dino@beansmile.com>
2019-04-02 00:17:37 +0800

    考试表单更新

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -6,12 +6,12 @@ class Paper < ApplicationRecord
   has_many :exams
 
   validates :title, presence: true, length: {maximum: 100}
-  validates_numericality_of :multiple_choice_count, greater_than: 5
-  validates_numericality_of :sentence_completion_count, greater_than: 5
-  validates_numericality_of :true_or_flase_question_count, greater_than: 5
-  validates_numericality_of :short_answer_question_count, greater_than: 1
+  validates_numericality_of :multiple_choice_count, greater_than_or_equal_to: 5
+  validates_numericality_of :sentence_completion_count, greater_than_or_equal_to: 5
+  validates_numericality_of :true_or_flase_question_count, greater_than_or_equal_to: 5
+  validates_numericality_of :short_answer_question_count, greater_than_or_equal_to: 1
 
-  validate :validate_question_number
+  validate :validate_question_number, on: :update
 
   accepts_nested_attributes_for :questions, allow_destroy: true
 
@@ -21,11 +21,13 @@ class Paper < ApplicationRecord
   enum review: {check: 0, spass: 1, fpass: 2 }
   enum level: {"入门": 0, "初级": 1, "中级": 2, "高级": 3}
 
-  before_create :generate_questions
+  after_create :generate_questions
 
   def generate_questions
-    question_ids = subject.questions.public_send(level).ids.sample(question_number).uniq
-    questions << Question.find(question_ids)
+    generate_multiple_choice_questions
+    generate_sentence_completion_questions
+    generate_true_or_flase_question_questions
+    generate_short_answer_question_questions
   end
 
   private
@@ -34,5 +36,25 @@ class Paper < ApplicationRecord
     errors.add(:sentence_completion_count, "题库中填空题题数量不足或未选择足够数量") if questions.exists? && (questions.sentence_completion.size < sentence_completion_count)
     errors.add(:true_or_flase_question_count, "题库中判断题题数量不足或未选择足够数量") if questions.exists? && (questions.true_or_flase_question.size < true_or_flase_question_count)
     errors.add(:short_answer_question_count, "题库中简单题题数量不足或未选择足够数量") if questions.exists? && (questions.short_answer_question.size < short_answer_question_count)
+  end
+
+  def generate_multiple_choice_questions
+    question_ids = subject.questions.multiple_choice.public_send(level).ids.uniq.sample(multiple_choice_count)
+    questions << Question.find(question_ids)
+  end
+
+  def generate_sentence_completion_questions
+    question_ids = subject.questions.sentence_completion.public_send(level).ids.uniq.sample(sentence_completion_count)
+    questions << Question.find(question_ids)
+  end
+
+  def generate_true_or_flase_question_questions
+    question_ids = subject.questions.true_or_flase_question.public_send(level).ids.uniq.sample(true_or_flase_question_count)
+    questions << Question.find(question_ids)
+  end
+
+  def generate_short_answer_question_questions
+    question_ids = subject.questions.short_answer_question.public_send(level).ids.uniq.sample(short_answer_question_count)
+    questions << Question.find(question_ids)
   end
 end
